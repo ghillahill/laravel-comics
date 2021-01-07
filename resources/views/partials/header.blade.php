3b2db136465453d00dd37dae50339ddf7c78a545
Alberto <thebertsinfo@gmail.com>
2021-01-07 16:33:15 +0100

    Header Bottom

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -12,6 +12,49 @@
         </div>
     </div>
     <div class="header-bottom">
+        <div class="container">
+            <nav>
+                <div class="logo">
+                    <img src="{{asset('images/dc-logo.png')}}" alt="">
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li>
+                            <a href="#">Characters</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a href="#">Tv</a>
+                        </li>
+                        <li>
+                            <a href="#">Games</a>
+                        </li>
+                        <li>
+                            <a href="#">Collectibles</a>
+                        </li>
+                        <li>
+                            <a href="#">Videos</a>
+                        </li>
+                        <li>
+                            <a href="#">Fans</a>
+                        </li>
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="search">
 
+                </div>
+            </nav>
+        </div>
     </div>
 </header>
